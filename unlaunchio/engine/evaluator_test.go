9ee08e637b89b3837a6d15eca7598030d27c70aa
Umer Mansoor <umermk3@gmail.com>
2021-01-26 21:14:30 -0800

    Added negative case

--- unlaunchio/engine/evaluator_test.go
+++ unlaunchio/engine/evaluator_test.go
@@ -23,10 +23,28 @@ func TestWhen_TargetRule_MultipleConditionsMatch(t *testing.T) {
 	v := e.matchTargetingRules(responseDto, "user123", &attributes)
 
 	if v == nil {
-		t.Errorf("Both conditions are not matched")
+		t.Errorf("Both conditions did not matched")
 	}
 
 	if v != nil && v.Key != "on" {
 		t.Errorf("Expected '%s'. Got '%s'", "on", v.Key)
+	}
+}
+
+func TestWhen_TargetRule_OneOfMultipleConditionsDoNotMatch(t *testing.T) {
+
+	var mockedFlag, _ = ioutil.ReadFile("../testdata/flagWithMultipleConditionRule.json")
+	var responseDto *dtos.Feature
+
+	attributes := make(map[string]interface{})
+	attributes["device"] = "ABCS"
+	attributes["registered"] = false // Let's not match this
+
+	json.Unmarshal(mockedFlag, &responseDto)
+
+	v := e.matchTargetingRules(responseDto, "user123", &attributes)
+
+	if v != nil {
+		t.Errorf("should not have matched")
 	}
 }
